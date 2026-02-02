48b40f4b907fb1b4ded4f1b8eb78c8cf162efea6
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-02-02 23:15:59 +0530

    Second commit

---
+++ DSA/Lecture 8/question_14.c
@@ -0,0 +1,37 @@
+//create a link list , display , insert a new node , delete a node , exit , these are choices 
+#include <stdio.h>
+
+int main() {
+    int choice;
+    do {
+        printf("\nMenu:\n");
+        printf("1. Create Linked List\n");
+        printf("2. Display Linked List\n");
+        printf("3. Insert a New Node\n");
+        printf("4. Delete a Node\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+if (choice == 1) {
+    printf("Creating Linked List...\n");
+}
+else if (choice == 2) {
+    printf("Displaying Linked List...\n");
+}
+else if (choice == 3) {
+    printf("Inserting a New Node...\n");
+}
+else if (choice == 4) {
+    printf("Deleting a Node...\n");
+}
+else if (choice == 5) {
+    printf("Exiting\n");
+}
+else {
+    printf("Invalid choice! Please try again.\n");
+}
+
+    } while(choice != 5);
+    return 0;
+}
