157f4c56d6ef1e8a4b9333797de26cd452b6fc8e
Nawanshu Sharma <nawanshusharma05@gmail.com>
2026-03-12 14:27:24 +0530

    Update App.js

--- IBM/Full stack/React/Introduction/my-app/src/App.js
+++ IBM/Full stack/React/Introduction/my-app/src/App.js
@@ -1,9 +1,32 @@
 function App(){
   return(
-    <div>
-      <h1>Hello World</h1>
-      <h2>My React App is working</h2>
-    </div>
+    
+    <div style={{textAlign: "center", backgroundColor:"white", height:"100vh", padding:"40px"}}>
+      <h1 style = {{color:"blue"}}>Welcome to My React App</h1>
+
+      <p style = {{fontSize:"18px", width:"600px", margin:"auto"}}>
+        React is a powerful JavaScript library for building user interfaces. It allows developers to create reusable , which makes website development faster.
+      </p>
+    
+    
+  
+
+      <div style={{
+        marginTop: "40px",
+        backgroundColor: "white",
+        padding: "20px",
+        borderRadius: "10px",
+        margin: "40px auto",
+        width: "300px",
+        boxShadow: "0 5px 10px rgba(0,0,0,0.2)",
+        color: "black"
+      }}>
+        <h2>Student Profile</h2>
+        <p><b>Name:</b> Nawanshu</p>
+        <p><b>Course:</b> BCA</p>
+        <p><b>University:</b> Lords University</p>
+      </div>
+      </div>
   );
 }
 
